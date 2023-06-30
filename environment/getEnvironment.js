e891c2e16aef8e6f5bb0fcca08ddd6bc2b479567
moises <angular.moises82@gmail.com>
2023-06-30 18:57:42 -0300

    elimine un archivo de sobra en la raiz del proyecto y corregi texto explicativo del modulo getEnvironment

--- environment/getEnvironment.js
+++ environment/getEnvironment.js
@@ -17,16 +17,16 @@
 
    /* explicacion de este modulo
 
-   en la carpeta environmentse pondran dos archivos cada uno tendra un objeto con propiedades
-   y valores de entorno, es importante que en ambos objetos de lso dos archivoslas propiedades
+  en la carpeta environment se pondran dos archivos cada uno tendra un objeto con propiedades
+   y valores de entorno, es importante que en ambos objetos de los dos archivos las propiedades
    se llamen de la misma manera, ya que luego en el codigo cuando se haga referencia
-   a esaspropeidadesse usaran esos nombres de las propiedades.
-   Luegoen el modulo getEnvironment cro una funcion que en un bloqueTryCatch
-   intenta en el primero importar el archivo config, este archivo se ignorara
-   en el gitnoinore por lo que cuando se suba a git no se subira, o sea que no estara
+   a esas propeidades se usaran esos nombres de las propiedades.
+   Luego en el modulo getEnvironment creo una funcion que en un bloqueTryCatch
+   intenta en el try importar el archivo config, este archivo se ignorara
+   en el gitignore  por lo que cuando se suba a git no se subira, o sea que no estara
    en produccion, cuando se intenta accedera este archivo dara false por lo que se pasara al catch 
-    es improtante notar que en ese archivo config estaran lso valores quemados, o sea losvalores
-    que se usaran en el entorno de desarrollo, cuando se pasa al catch se buscara improtar otro archivo
-    que este si estara en el entornode desarrollo y simplemente tiene el mismo objeto con las mismas
+    es improtante notar que en ese archivo config estaran los valores quemados, o sea los valores
+    que se usaran en el entorno de desarrollo, cuando se pasa al catch se buscara importar otro archivo
+    que  simplemente tiene el mismo objeto con las mismas
     propiedades pero los valores de estas propiedades son variablesde entornos que se pasaran cuando 
     el entorno de produccion pida esos valores */
